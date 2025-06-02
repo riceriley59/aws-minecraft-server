bd12e13014e1995eec1f758fe2d88142e7b9ab51
Riley Rice <riceriley59@gmail.com>
2025-06-02 14:06:21 -0700

    Added remote state storage

---
+++ bootstrap-state-storage/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "riceri-terraform-state-bucket"
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "riceri-terraform-state-bucket"
+    key    = "minecraft/terraform.tfstate"
+    region = "us-west-2"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
